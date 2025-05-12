859e09974fd514d9600f37c0a31e52d17873eb71
Hu ChiHuoy <ciscyl191255@outlook.com>
2025-05-12 14:53:30 +0900

    cloudflare page deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SubdomainList from "./components/SubdomainList";
+import AddSubdomainForm from "./components/AddSubdomainForm";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-header">
+        <div className="flex justify-center">
+          <img
+            src="/logo.png"
+            alt="Cloudflare Logo"
+            className="h-16 w-16"
+          />
+        </div>
+ 
+        
+        <h1 >Cloudflare Subdomain Admin</h1>
+        <Routes>
+          <Route path="/" element={<SubdomainList />} />
+          <Route path="/add" element={<AddSubdomainForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
